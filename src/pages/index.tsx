8020dd485696840613151d30016c026993c7a7e1
Joseph <40701096+oudajosefu@users.noreply.github.com>
2022-08-29 16:02:17 -0700

    Fix screen sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,17 +21,17 @@ const Home: NextPage = () => {
     }, [bill, tipPercent, people]);
 
     return (
-        <div>
+        <>
             <Head>
                 <title>Tip Calculator</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel='apple-touch-icon' href='/apple-icon.png' />
             </Head>
 
-            <main className="grid w-screen h-screen grid-rows-[auto_1fr] lg:flex lg:flex-col lg:items-center bg-grayish-cyan-200 lg:justify-center">
+            <main className="grid min-h-screen grid-rows-[auto_1fr] lg:flex lg:flex-col lg:items-center bg-grayish-cyan-200 lg:justify-center">
                 <Header height='hidden md:flex h-36 lg:h-auto lg:-mt-20' />
-                <div className='flex flex-col justify-center min-h-screen gap-5 bg-white md:min-h-fit md:rounded-t-3xl p-7 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11'>
-                    <div className='flex flex-col gap-5 lg:gap-0 lg:justify-between'>
+                <div className='flex flex-col justify-between min-h-screen bg-white md:min-h-fit md:rounded-t-3xl p-7 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11'>
+                    <div className='flex flex-1 justify-evenly flex-col lg:justify-between'>
                         <Bill setBill={setBill} setResetState={setResetState} />
                         <Tips setTipPercent={setTipPercent} />
                         <People
@@ -48,7 +48,7 @@ const Home: NextPage = () => {
                     />
                 </div>
             </main>
-        </div>
+        </>
     );
 };
 
