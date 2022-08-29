0bce73d473a9e973122bac190a1b04ca3635ea5e
Joseph <40701096+oudajosefu@users.noreply.github.com>
2022-08-29 16:37:11 -0700

    Justified to center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
                 <link rel='apple-touch-icon' href='/apple-icon.png' />
             </Head>
 
-            <main className="flex flex-col min-h-screen lg:items-center bg-grayish-cyan-200 lg:justify-center">
+            <main className="flex flex-col min-h-screen lg:items-center bg-grayish-cyan-200 justify-center">
                 <Header height='hidden md:flex h-36 lg:h-auto lg:-mt-20' />
                 <div className='flex flex-col justify-start rounded-3xl bg-white md:min-h-fit md:rounded-t-3xl px-7 py-4 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11 gap-4'>
                     <div className='flex flex-auto gap-2 flex-col justify-evenly'>
