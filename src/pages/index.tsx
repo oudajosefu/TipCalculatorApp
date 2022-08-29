64d8da00d1eb5e18805f99ae0e6ae547bf8b96e2
Joseph <40701096+oudajosefu@users.noreply.github.com>
2022-08-29 16:30:56 -0700

    One more time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ const Home: NextPage = () => {
 
             <main className="grid min-h-screen md:grid-rows-[auto_1fr] lg:flex lg:flex-col lg:items-center bg-grayish-cyan-200 lg:justify-center">
                 <Header height='hidden md:flex h-36 lg:h-auto lg:-mt-20' />
-                <div className='flex flex-col rounded-3xl bg-white md:min-h-fit md:rounded-t-3xl px-7 py-4 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11 gap-2'>
-                    <div className='flex flex-auto gap-2 flex-col justify-evenly'>
+                <div className='flex flex-col rounded-3xl bg-white md:min-h-fit md:rounded-t-3xl px-7 py-4 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11 gap-4'>
+                    <div className='flex flex-auto gap-2 flex-col justify-between'>
                         <Bill setBill={setBill} setResetState={setResetState} />
                         <Tips setTipPercent={setTipPercent} />
                         <People
