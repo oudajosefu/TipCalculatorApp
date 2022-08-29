4a45cbc25859fc8cc2ee536990aeb3c17287ad94
Joseph <40701096+oudajosefu@users.noreply.github.com>
2022-08-29 16:27:36 -0700

    Fixed screen sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,10 +28,10 @@ const Home: NextPage = () => {
                 <link rel='apple-touch-icon' href='/apple-icon.png' />
             </Head>
 
-            <main className="grid min-h-screen grid-rows-[auto_1fr] lg:flex lg:flex-col lg:items-center bg-grayish-cyan-200 lg:justify-center">
+            <main className="grid min-h-screen md:grid-rows-[auto_1fr] lg:flex lg:flex-col lg:items-center bg-grayish-cyan-200 lg:justify-center">
                 <Header height='hidden md:flex h-36 lg:h-auto lg:-mt-20' />
-                <div className='flex flex-col justify-between min-h-screen bg-white md:min-h-fit md:rounded-t-3xl p-7 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11'>
-                    <div className='flex flex-1 justify-evenly flex-col lg:justify-between'>
+                <div className='flex flex-col rounded-3xl bg-white md:min-h-fit md:rounded-t-3xl px-7 py-4 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11 gap-2'>
+                    <div className='flex flex-auto gap-2 flex-col justify-evenly'>
                         <Bill setBill={setBill} setResetState={setResetState} />
                         <Tips setTipPercent={setTipPercent} />
                         <People
