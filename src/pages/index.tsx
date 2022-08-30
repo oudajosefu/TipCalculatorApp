e2ff62ba16f6196a1792333ae5309f298454e5f9
Joseph <40701096+oudajosefu@users.noreply.github.com>
2022-08-29 22:33:44 -0700

    Updated styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,9 +28,9 @@ const Home: NextPage = () => {
                 <link rel='apple-touch-icon' href='/apple-icon.png' />
             </Head>
 
-            <main className="flex flex-col min-h-screen lg:items-center bg-grayish-cyan-200 justify-start">
+            <main className="flex flex-col min-h-screen lg:items-center bg-grayish-cyan-200 justify-start lg:justify-center">
                 <Header height='flex h-12 md:h-36 lg:h-auto lg:-mt-20' />
-                <div className='flex flex-col justify-start rounded-3xl bg-white md:min-h-fit md:rounded-t-3xl p-7 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11 gap-4'>
+                <div className='flex flex-col justify-evenly rounded-3xl bg-white md:min-h-fit md:rounded-t-3xl p-7 lg:p-11 lg:w-2/3 2xl:w-1/2 lg:rounded-3xl lg:flex-row lg:mt-16 lg:h-1/2 lg:gap-11 gap-4'>
                     <div className='flex flex-auto gap-2 flex-col justify-evenly'>
                         <Bill setBill={setBill} setResetState={setResetState} />
                         <Tips setTipPercent={setTipPercent} />
